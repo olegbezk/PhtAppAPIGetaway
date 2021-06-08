1573b42b7cbce9ce81d5a2e48019fe8bb9105a4b
Oleg Bezkorovaynyi <oleg.bezkorovajniy@gmail.com>
2021-06-08 12:13:39 +0300

    initial

---
+++ src/main/java/com/api/geteway/PhAppApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.api.geteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhAppApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhAppApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/geteway/PhAppApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.geteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhAppApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
